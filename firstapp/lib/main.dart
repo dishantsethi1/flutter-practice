4ae29db73bfb18bd9dfdfd7a0241b0bcec1eaa20
Dishant Sethi <dishantsethi266@gmail.com>
2020-06-14 23:47:41 +0530

    quiz app

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main(){
-runApp(MyApp());
+void main() {
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var question = 0;
+  void answer() {
+    setState(() {
+        question = question + 1;
+    });
+  
+    print(question);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var q = ['what is your name', 'what is your age'];
     return MaterialApp(
-      home:Scaffold(
+      home: Scaffold(
         appBar: AppBar(
-          title: Text('my first app'),
+          title: Text('my first flutter app '),
         ),
-        body: Text('hi i am dishant'),
-    ),
+        body: Column(
+          children: [
+            Text(
+              q[question],
+            ),
+            RaisedButton(
+              child: Text('1 '),
+              onPressed: answer,
+            ),
+            RaisedButton(
+              child: Text('1'),
+              onPressed: answer,
+            ),
+            RaisedButton(
+              child: Text('1'),
+              onPressed: answer,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
